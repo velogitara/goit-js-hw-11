3fd12e71cf61a375e1c078c03f86ea46297a4321
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-06-07 11:28:40 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import GalleryApiService from './components/gallery-API-service';
-const axios = require('axios').default;
-
 import LoadMoreBtn from './components/loadMoreBtn';
 
 const API_KEY = '27573462-7cfd1b03d2f186a851a1b1b26';
@@ -52,13 +52,21 @@ async function onClick(e) {
     } else {
       const result = await galleryApi.fetchImages();
       const { data } = result;
-
+      console.log(data);
       if (data.hits.length === 0) {
         Notiflix.Report.failure('Sorry We did not find anything', {
           messageFontSize: '20px',
           messageMaxLength: 1923,
           plainText: false,
         });
+      } else if (data.hits.length < 40) {
+        Notiflix.Notify.success(`Hooray! We found ${data.total} images.`);
+        loadMoreBtn.show();
+        loadMoreBtn.disable();
+        setTimeout(() => {
+          Refs.galleryRef.innerHTML = createCard(data.hits);
+          loadMoreBtn.hide();
+        }, 1000);
       } else {
         Notiflix.Notify.success(`Hooray! We found ${data.total} images.`);
         loadMoreBtn.show();
@@ -66,6 +74,7 @@ async function onClick(e) {
         setTimeout(() => {
           Refs.galleryRef.innerHTML = createCard(data.hits);
           loadMoreBtn.enable();
+          lightbox.refresh();
         }, 1000);
       }
     }
@@ -77,12 +86,14 @@ async function onClick(e) {
 async function onLoadMore() {
   const result = await galleryApi.loadMore();
   const { data } = result;
-  console.log(data.hits.length);
-  if (data.hits.length < 40) {
+  console.log(galleryApi.page);
+  if (data.hits.length < 40 && galleryApi.page > 1) {
     loadMoreBtn.hide();
-    Notiflix.Report.warning('No More Pictures To Load', 'We are very sorry');
     Refs.backToTopBtn.classList.remove('is-hidden');
     Refs.galleryRef.insertAdjacentHTML('beforeend', createCard(data.hits));
+    setTimeout(() => {
+      Notiflix.Notify.warning('Мы нашли все картинки, что смогли ');
+    }, 3000);
   } else {
     loadMoreBtn.show();
     loadMoreBtn.disable();
@@ -98,7 +109,7 @@ function createCard(items) {
   return items
     .map(item => {
       return `<div class="photo-card">
-              <div class="thumb"><img src="${item.webformatURL}" class="image" alt="${item.tags}" loading="lazy" /></div>
+              <div class="thumb"><a href="${item.largeImageURL}"><img src="${item.webformatURL}" class="image" alt="${item.tags}" loading="lazy" /></a></div>
               <div class="info">
                   <p class="info-item">
                       <b>Likes:<span class="info-item__value">${item.likes}</span></b>
@@ -128,3 +139,16 @@ function backToTop() {
     setTimeout(backToTop, 0);
   }
 }
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+lightbox.on('show.simplelightbox', function () {
+  // Do something…
+});
+lightbox.on('error.simplelightbox', function (e) {
+  console.log(e); // Some usefull information
+});
